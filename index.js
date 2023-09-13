73277720de77220a06c58f3135cc53b2edff4e6c
dovenci <dogevenci@gmail.com>
2023-09-13 15:56:10 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import puppeteer from 'puppeteer';
 import * as fs from "fs"
+import * as path from "path"
+
+const sleep = (milliseconds) => new Promise(r => setTimeout(r, milliseconds));
 
 const main = async () => {
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
+        userDataDir: path.resolve(process.cwd(), "userdata"),
         args: [
             '--proxy-server=socks5://127.0.0.1:7777',
         ],
@@ -17,13 +21,14 @@ const main = async () => {
     });
     if (data) {
         const imgData = data.replace(/^data:image\/\w+;base64,/, "");
-        var dataBuffer = Buffer.from(imgData, 'base64');
+        let dataBuffer = Buffer.from(imgData, 'base64');
         fs.writeFileSync("image.png", dataBuffer)
         console.log("二维码保存成功 扫码登录", "image.png")
     }
     await page.waitForSelector("#globalNav > div.header_rightWrapper__2CIJ_ > div:nth-child(3) > div > div > a > span", { timeout: 999999 })
     await page.click("#globalNav > div.header_rightWrapper__2CIJ_ > div:nth-child(3) > div > div > a > span")
     await page.click("#globalNav > div.header_rightWrapper__2CIJ_ > div:nth-child(3) > div > div > div > div > section > ul > li:nth-child(5) > a")
+    await sleep(1000)
 }
 
 main()
